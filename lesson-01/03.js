d39696ce8a27ec575090a5a1b0e0edae54ed1627
Dennesota <Dennersota@gmail.com>
2024-07-08 18:30:02 +0300

    tt

---
+++ lesson-01/03.js
@@ -0,0 +1,6 @@
+let number = 1;
+
+console.log(number++); // 1: сначала выводит number, затем увеличивает его на 1 (number становится 2)
+console.log(++number); // 3: сначала увеличивает number на 1 (number становится 3), затем выводит его
+console.log(number--); // 3: сначала выводит number, затем уменьшает его на 1 (number становится 2)
+console.log(--number); // 1: сначала уменьшает number на 1 (number становится 1), затем выводит его
