11b5a266bb20d9932bf609c24a664ba271e0e0d0
Dennesota <Dennersota@gmail.com>
2024-07-08 18:23:22 +0300

    test 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,11 @@
 // code
+// Генерация случайного числа от 0 до 99
+let number = Math.floor(Math.random() * 100);
+
+// Проверка на четность и вывод результата в консоль
+if (number % 2 === 0) {
+    console.log(`${number} : Это четное число`);
+} else {
+    console.log(`${number} : Это нечетное число`);
+}
+
