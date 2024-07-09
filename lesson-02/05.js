946278551cb7ab8c973d35c05f5816450eb3235c
Dennesota <Dennersota@gmail.com>
2024-07-09 14:46:15 +0300

    пп

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,14 +1,12 @@
-//Дан объект passport. Создай его копию и поменяй имя с "Petr" на "Ivan",
-//и убедись в консоли, что у объекта passport имя осталось "Petr", а у твоего нового объекта "Ivan".
-
 let passport = {
     name: "Petr",
     surname: "Petrov",
 };
 
 // Создаем копию объекта и изменяем свойство name
-let newPassport = {...passport, name: "Ivan"};
+let Passport1 = { ...passport, name: "Ivan" };
 
 // Проверяем значения в консоли
 console.log(passport); // { name: "Petr", surname: "Petrov" }
-console.log(newPassport); // { name: "Ivan", surname: "Petrov" }
+console.log(Passport1); // { name: "Ivan", surname: "Petrov" }
+
